ef4f522cb0f1078db33a92866a4a4bd6ce5866a6
余佐 <yuzuo1216@gmail.com>
2019-03-06 08:20:24 +0800

    ADD:使用腾讯词向量创建EmbeddingLayer

--- yznlp/wordvec.py
+++ yznlp/wordvec.py
@@ -1,6 +1,9 @@
 #encoding:utf-8
 import numpy as np
 def load_tencent_word_embedding(path):
+    '''加载腾讯词向量.
+    @return embeddings, dim
+    '''
     words = {}
     fp = open(path, 'r')
     line = fp.readline()
@@ -8,15 +11,17 @@ def load_tencent_word_embedding(path):
     
     while True:
         line = fp.readline()
+        line = line.strip()
+        if len(line) == 0:
+            break
         word, floats =  line.split(' ',1)
         words[word] = np.fromstring(floats, dtype=np.float, count=200, sep=' ')
         count += 1
-    return words
+    return words,200
 
     
     
 if __name__ == '__main__':
     words = load_tencent_word_embedding('../var/Tencent_AILab_ChineseEmbedding.txt')
-    print(words)
     
 
